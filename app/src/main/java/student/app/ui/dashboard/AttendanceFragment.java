739b9f2381c4807c1e1065c6fd337c0be8e52d49
oyamoh-brian <oyamobrian8@gmail.com>
2020-11-03 19:28:27 +0300

    nfc

--- app/src/main/java/student/app/ui/dashboard/AttendanceFragment.java
+++ app/src/main/java/student/app/ui/dashboard/AttendanceFragment.java
@@ -37,7 +37,7 @@ import static android.os.Looper.getMainLooper;
  * Use the {@link AttendanceFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AttendanceFragment extends Fragment implements View.OnClickListener{
+public class AttendanceFragment extends NFCFragment implements View.OnClickListener{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,30 +53,6 @@ public class AttendanceFragment extends Fragment implements View.OnClickListener
     TextView tv;
     EditText editText;
     ProgressBar pb;
-    WifiP2pManager manager;
-    WifiP2pManager.Channel channel;
-    IntentFilter intentFilter;
-    NFCBroadcastReceiver receiver;
-    private boolean isWifiP2pEnabled = false;
-    private boolean retryChannel = false;
-
-
-    final int FL_C = 0;
-    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
-        this.isWifiP2pEnabled = isWifiP2pEnabled;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                                           int[] grantResults) {
-        switch (requestCode) {
-            case FL_C:
-                if  (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                    Log.e(TAG, "Fine location permission is not granted!");
-                }
-                break;
-        }
-    }
 
 
     public AttendanceFragment() {
@@ -110,40 +86,6 @@ public class AttendanceFragment extends Fragment implements View.OnClickListener
         }
     }
 
-    private boolean initP2p() {
-        // Device capability definition check
-        if (!getContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI_DIRECT)) {
-            Log.e(TAG, "Wi-Fi Direct is not supported by this device.");
-            return false;
-        }
-        // Hardware capability check
-        WifiManager wifiManager = (WifiManager) getContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        if (wifiManager == null) {
-            Log.e(TAG, "Cannot get Wi-Fi system service.");
-            return false;
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            if (!wifiManager.isP2pSupported()) {
-                Log.e(TAG, "Wi-Fi Direct is not supported by the hardware or Wi-Fi is off.");
-                return false;
-            }
-        }else{
-            return false;
-        }
-        manager = (WifiP2pManager) getContext().getSystemService(Context.WIFI_P2P_SERVICE);
-        if (manager == null) {
-            Log.e(TAG, "Cannot get Wi-Fi Direct system service.");
-            return false;
-        }
-        channel = manager.initialize(getContext(), getMainLooper(), null);
-        if (channel == null) {
-            Log.e(TAG, "Cannot initialize Wi-Fi Direct.");
-            return false;
-        }
-        return true;
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -156,46 +98,6 @@ public class AttendanceFragment extends Fragment implements View.OnClickListener
         na = v.findViewById(R.id.notAllowed);
         pb = v.findViewById(R.id.progress);
         scan.setOnClickListener(this);
-        intentFilter = new IntentFilter();
-        // Indicates a change in the wifi P2p status
-        intentFilter.addAction(WIFI_P2P_STATE_CHANGED_ACTION);
-
-        // Indicates change in the list of available peers
-        intentFilter.addAction(WIFI_P2P_PEERS_CHANGED_ACTION);
-
-        // Indicates the state of Wifi p2p connectivity has changed
-        intentFilter.addAction(WIFI_P2P_CONNECTION_CHANGED_ACTION);
-
-        // Indicates this device's de tails have changed
-        intentFilter.addAction(WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        // Assign to the manager
-        manager = (WifiP2pManager) getContext().getSystemService(Context.WIFI_P2P_SERVICE);
-        receiver = new NFCBroadcastReceiver();
-        receiver.setManager(manager);
-        receiver.setChannel(channel);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
-                && getContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FL_C);
-            // After this point you wait for callback in
-            // onRequestPermissionsResult(int, String[], int[]) overridden method
-        }
-        if(manager != null){
-            channel = manager.initialize(getContext(), getMainLooper(), null);
-            manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
-                @Override
-                public void onSuccess() {
-                    Toast.makeText(getContext(), "RUnnoign", Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onFailure(int reason) {
-                    Toast.makeText(getContext(), "Err"+reason, Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
 
         AuthPref authPref = new AuthPref(this.getContext());
         String userGroup = authPref.getUserGroup();
@@ -231,18 +133,4 @@ public class AttendanceFragment extends Fragment implements View.OnClickListener
         }
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        getContext().unregisterReceiver(receiver);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        receiver = new NFCBroadcastReceiver();
-        receiver.setManager(manager);
-        receiver.setChannel(channel);
-        getContext().registerReceiver(receiver, intentFilter);
-    }
 }
--- app/src/main/java/student/app/ui/dashboard/Home.java
+++ app/src/main/java/student/app/ui/dashboard/Home.java
@@ -7,10 +7,24 @@ import androidx.core.text.HtmlCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.Spanned;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -24,19 +38,97 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+
+
 import student.app.R;
 import student.app.Splash;
 import student.app.livedata.StudentData;
 import student.app.models.Student;
 import student.app.ui.Pager;
+import student.app.util.NFCBroadcastReceiver;
 
-public class Home extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
+public class Home extends AppCompatActivity implements
+        TabLayout.OnTabSelectedListener,
+        WifiP2pManager.ChannelListener,
+        WifiP2pManager.ConnectionInfoListener,
+        WifiP2pManager.PeerListListener {
     private StudentData studentData;
     FirebaseAuth auth;
     FirebaseFirestore db;
     TabLayout tl;
     MaterialToolbar tb;
     ViewPager viewPager;
+
+    // Networking
+    public static final String TAG = "HOME";
+    private static final int PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION = 1001;
+    private WifiP2pManager manager;
+    private boolean isWifiP2pEnabled = false;
+    private boolean retryChannel = false;
+    private final IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager.Channel channel;
+
+    // Broadcast receiver
+    private NFCBroadcastReceiver receiver = null;
+
+
+    // Detected gadget
+    private WifiP2pDevice device;
+    private WifiP2pInfo info;
+
+
+
+    /**
+     * @param isWifiP2pEnabled the isWifiP2pEnabled to set
+     */
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION:
+                if  (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.e(TAG, "Fine location permission is not granted!");
+                }
+                break;
+        }
+    }
+    private boolean initP2p() {
+        // Device capability definition check
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI_DIRECT)) {
+            Log.e(TAG, "Wi-Fi Direct is not supported by this device.");
+            return false;
+        }
+        // Hardware capability check
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifiManager == null) {
+            Log.e(TAG, "Cannot get Wi-Fi system service.");
+            return false;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (!wifiManager.isP2pSupported()) {
+                Log.e(TAG, "Wi-Fi Direct is not supported by the hardware or Wi-Fi is off.");
+                return false;
+            }
+        }else{
+            return false;
+        }
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        if (manager == null) {
+            Log.e(TAG, "Cannot get Wi-Fi Direct system service.");
+            return false;
+        }
+        channel = manager.initialize(this, getMainLooper(), null);
+        if (channel == null) {
+            Log.e(TAG, "Cannot initialize Wi-Fi Direct.");
+            return false;
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,7 +146,88 @@ public class Home extends AppCompatActivity implements TabLayout.OnTabSelectedLi
         tl.setupWithViewPager(viewPager);
         tl.setOnTabSelectedListener(this);
 
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+
+        if (manager != null && channel != null) {
+            // Since this is the system wireless settings activity, it's
+            // not going to send us a result. We will be notified by
+            // WiFiDeviceBroadcastReceiver instead.
+
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setMessage("You need to enable WI-FI/NFC or this app will exit.");
+            alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+
+            alertDialog.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+                }
+            });
+
+        } else {
+            Log.e(TAG, "channel or manager is null");
+        }
+
+
+
+        // SDK
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION);
+            // After this point you wait for callback in
+            // onRequestPermissionsResult(int, String[], int[]) overridden method
+        }else{
+            if (!initP2p()) {
+                finish();
+            }else{
+                startListening();
+            }
+        }
+
+
+
     }
+
+
+    void startListening(){
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(Home.this, "Discovery Initiated",
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onFailure(int reasonCode) {
+                Toast.makeText(Home.this, "Discovery Failed : " + reasonCode,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        receiver = new NFCBroadcastReceiver(manager, channel, this);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
     Spanned html(String ht){
         return HtmlCompat.fromHtml(ht, HtmlCompat.FROM_HTML_MODE_LEGACY);
     }
@@ -132,5 +305,55 @@ public class Home extends AppCompatActivity implements TabLayout.OnTabSelectedLi
     @Override
     public void onTabReselected(TabLayout.Tab tab) {
 
+    }
+
+
+    @Override
+    public void onChannelDisconnected() {
+
+    }
+
+
+
+    // The interface from the big class
+
+//    @Override
+//    public void showDetails(WifiP2pDevice device) {
+//
+//    }
+//
+//    @Override
+//    public void cancelDisconnect() {
+//
+//    }
+//
+//    @Override
+//    public void connect(WifiP2pConfig config) {
+//
+//    }
+//
+//    @Override
+//    public void disconnect() {
+//
+//    }
+
+    /**
+     * The requested connection info is available
+     *
+     * @param info Wi-Fi p2p connection info
+     */
+    @Override
+    public void onConnectionInfoAvailable(WifiP2pInfo info) {
+        this.info = info;
+    }
+
+    /**
+     * The requested peer list is available
+     *
+     * @param peers List of available peers
+     */
+    @Override
+    public void onPeersAvailable(WifiP2pDeviceList peers) {
+        Toast.makeText(this, peers.toString(), Toast.LENGTH_SHORT).show();
     }
 }
